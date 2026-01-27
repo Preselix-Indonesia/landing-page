44efdb7bc4db8cfbed2c0726959503d64da06853
naimshihabdev <naimshihab.dev@gmail.com>
2026-01-27 18:08:01 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  ...(isProd && {
-    output: "export",
-  }),
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
